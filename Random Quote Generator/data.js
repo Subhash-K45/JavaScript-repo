bb73a6b134340ce46418dcce7d6cd32430a34766
Subhash-K45 <128983855+Subhash-K45@users.noreply.github.com>
2023-03-30 20:15:06 +0530

    Add files via upload

---
+++ Random Quote Generator/data.js
@@ -0,0 +1,302 @@
+export const data=[{
+    0: 
+    {text: 'Genius is one percent inspiration and ninety-nine percent perspiration.', author: 'Thomas Edison'},
+    1
+    : 
+    {text: 'You can observe a lot just by watching.', author: 'Yogi Berra'},
+    2
+    : 
+    {text: 'A house divided against itself cannot stand.', author: 'Abraham Lincoln'},
+    3
+    : 
+    {text: 'Difficulties increase the nearer we get to the goal.', author: 'Johann Wolfgang von Goethe'},
+    4
+    : 
+    {text: 'Fate is in your hands and no one elses', author: 'Byron Pulsifer'},
+    5
+    : 
+    {text: 'Be the chief but never the lord.', author: 'Lao Tzu'},
+    6
+    : 
+    {text: 'Nothing happens unless first we dream.', author: 'Carl Sandburg'},
+    7
+    : 
+    {text: 'Well begun is half done.', author: 'Aristotle'},
+    8
+    : 
+    {text: 'Life is a learning experience, only if you learn.', author: 'Yogi Berra'},
+    9
+    : 
+    {text: 'Self-complacency is fatal to progress.', author: 'Margaret Sangster'},
+    10
+    : 
+    {text: 'Peace comes from within. Do not seek it without.', author: 'Buddha'},
+    11
+    : 
+    {text: 'What you give is what you get.', author: 'Byron Pulsifer'},
+    12
+    : 
+    {text: 'We can only learn to love by loving.', author: 'Iris Murdoch'},
+    13
+    : 
+    {text: 'Life is change. Growth is optional. Choose wisely.', author: 'Karen Clark'},
+    14
+    : 
+    {text: "You'll see it when you believe it.", author: 'Wayne Dyer'},
+    15
+    : 
+    {text: 'Today is the tomorrow we worried about yesterday.', author: null},
+    16
+    : 
+    {text: "It's easier to see the mistakes on someone else's paper.", author: null},
+    17
+    : 
+    {text: 'Every man dies. Not every man really lives.', author: null},
+    18
+    : 
+    {text: 'To lead people walk behind them.', author: 'Lao Tzu'},
+    19
+    : 
+    {text: 'Having nothing, nothing can he lose.', author: 'William Shakespeare'},
+    20
+    : 
+    {text: 'Trouble is only opportunity in work clothes.', author: 'Henry J. Kaiser'},
+    21
+    : 
+    {text: 'A rolling stone gathers no moss.', author: 'Publilius Syrus'},
+    22
+    : 
+    {text: 'Ideas are the beginning points of all fortunes.', author: 'Napoleon Hill'},
+    23
+    : 
+    {text: 'Everything in life is luck.', author: 'Donald Trump'},
+    24
+    : 
+    {text: 'Doing nothing is better than being busy doing nothing.', author: 'Lao Tzu'},
+    25
+    : 
+    {text: 'Trust yourself. You know more than you think you do.', author: 'Benjamin Spock'},
+    26
+    : 
+    {text: 'Study the past, if you would divine the future.', author: 'Confucius'},
+    27
+    : 
+    {text: 'The day is already blessed, find peace within it.', author: null},
+    28
+    : 
+    {text: 'From error to error one discovers the entire truth.', author: 'Sigmund Freud'},
+    29
+    : 
+    {text: 'Well done is better than well said.', author: 'Benjamin Franklin'},
+    30
+    : 
+    {text: 'Bite off more than you can chew, then chew it.', author: 'Ella Williams'},
+    31
+    : 
+    {text: 'Work out your own salvation. Do not depend on others.', author: 'Buddha'},
+    32
+    : 
+    {text: 'One today is worth two tomorrows.', author: 'Benjamin Franklin'},
+    33
+    : 
+    {text: 'Once you choose hope, anythings possible.', author: 'Christopher Reeve'},
+    34
+    : 
+    {text: 'God always takes the simplest way.', author: 'Albert Einstein'},
+    35
+    : 
+    {text: 'One fails forward toward success.', author: 'Charles Kettering'},
+    36
+    : 
+    {text: 'From small beginnings come great things.', author: null},
+    37
+    : 
+    {text: 'Learning is a treasure that will follow its owner everywhere', author: 'Chinese proverb'},
+    38
+    : 
+    {text: 'Be as you wish to seem.', author: 'Socrates'},
+    39
+    : 
+    {text: 'The world is always in movement.', author: 'V. Naipaul'},
+    40
+    : 
+    {text: 'Never mistake activity for achievement.', author: 'John Wooden'},
+    41
+    : 
+    {text: 'What worries you masters you.', author: 'Haddon Robinson'},
+    42
+    : 
+    {text: 'One faces the future with ones past.', author: 'Pearl Buck'},
+    43
+    : 
+    {text: 'Goals are the fuel in the furnace of achievement.', author: 'Brian Tracy'},
+    44
+    : 
+    {text: 'Who sows virtue reaps honour.', author: 'Leonardo da Vinci'},
+    45
+    : 
+    {text: 'Be kind whenever possible. It is always possible.', author: 'Dalai Lama'},
+    46
+    : 
+    {text: "Talk doesn't cook rice.", author: 'Chinese proverb'},
+    47
+    : 
+    {text: 'He is able who thinks he is able.', author: 'Buddha'},
+    48
+    : 
+    {text: 'A goal without a plan is just a wish.', author: 'Larry Elder'},
+    49
+    : 
+    {text: 'To succeed, we must first believe that we can.', author: 'Michael Korda'},
+    50
+    : 
+    {text: 'Learn from yesterday, live for today, hope for tomorrow.', author: 'Albert Einstein'},
+    51
+    : 
+    {text: 'A weed is no more than a flower in disguise.', author: 'James Lowell'},
+    52
+    : 
+    {text: 'Do, or do not. There is no try.', author: 'Yoda'},
+    53
+    : 
+    {text: 'All serious daring starts from within.', author: 'Harriet Beecher Stowe'},
+    54
+    : 
+    {text: 'The best teacher is experience learned from failures.', author: 'Byron Pulsifer'},
+    55
+    : 
+    {text: 'Think how hard physics would be if particles could think.', author: 'Murray Gell-Mann'},
+    56
+    : 
+    {text: "Love is the flower you've got to let grow.", author: 'John Lennon'},
+    57
+    : 
+    {text: "Don't wait. The time will never be just right.", author: 'Napoleon Hill'},
+    58
+    : 
+    {text: 'Time is the wisest counsellor of all.', author: 'Pericles'},
+    59
+    : 
+    {text: 'You give before you get.', author: 'Napoleon Hill'},
+    60
+    : 
+    {text: 'Wisdom begins in wonder.', author: 'Socrates'},
+    61
+    : 
+    {text: 'Without courage, wisdom bears no fruit.', author: 'Baltasar Gracian'},
+    62
+    : 
+    {text: 'Change in all things is sweet.', author: 'Aristotle'},
+    63
+    : 
+    {text: 'What you fear is that which requires action to overcome.', author: 'Byron Pulsifer'},
+    64
+    : 
+    {text: 'When performance exceeds ambition, the overlap is called success.', author: 'Cullen Hightower'},
+    65
+    : 
+    {text: 'When deeds speak, words are nothing.', author: 'African proverb'},
+    66
+    : 
+    {text: 'Real magic in relationships means an absence of judgement of others.', author: 'Wayne Dyer'},
+    67
+    : 
+    {text: 'I never think of the future. It comes soon enough.', author: 'Albert Einstein'},
+    68
+    : 
+    {text: 'Skill to do comes of doing.', author: 'Ralph Emerson'},
+    69
+    : 
+    {text: 'Wisdom is the supreme part of happiness.', author: 'Sophocles'},
+    70
+    : 
+    {text: 'I believe that every person is born with talent.', author: 'Maya Angelou'},
+    71
+    : 
+    {text: 'Important principles may, and must, be inflexible.', author: 'Abraham Lincoln'},
+    72
+    : 
+    {text: 'The undertaking of a new action brings new strength.', author: 'Richard Evans'},
+    73
+    : 
+    {text: 'The years teach much which the days never know.', author: 'Ralph Emerson'},
+    74
+    : 
+    {text: 'Our distrust is very expensive.', author: 'Ralph Emerson'},
+    75
+    : 
+    {text: 'All know the way; few actually walk it.', author: 'Bodhidharma'},
+    76
+    : 
+    {text: 'Great talent finds happiness in execution.', author: 'Johann Wolfgang von Goethe'},
+    77
+    : 
+    {text: 'Faith in oneself is the best and safest course.', author: 'Michelangelo'},
+    78
+    : 
+    {text: 'Courage is going from failure to failure without losing enthusiasm.', author: 'Winston Churchill'},
+    79
+    : 
+    {text: 'The two most powerful warriors are patience and time.', author: 'Leo Tolstoy'},
+    80
+    : 
+    {text: 'Anticipate the difficult by managing the easy.', author: 'Lao Tzu'},
+    81
+    : 
+    {text: 'Those who are free of resentful thoughts surely find peace.', author: 'Buddha'},
+    82
+    : 
+    {text: 'A short saying often contains much wisdom.', author: 'Sophocles'},
+    83
+    : 
+    {text: 'It takes both sunshine and rain to make a rainbow.', author: null},
+    84
+    : 
+    {text: 'A beautiful thing is never perfect.', author: null},
+    85
+    : 
+    {text: 'Only do what your heart tells you.', author: 'Princess Diana'},
+    86
+    : 
+    {text: 'Life is movement-we breathe, we eat, we walk, we move!', author: 'John Pierrakos'},
+    87
+    : 
+    {text: 'No one can make you feel inferior without your consent.', author: 'Eleanor Roosevelt'},
+    88
+    : 
+    {text: 'Argue for your limitations, and sure enough theyre yours.', author: 'Richard Bach'},
+    89
+    : 
+    {text: 'Luck is what happens when preparation meets opportunity.', author: 'Seneca'},
+    90
+    : 
+    {text: 'Victory belongs to the most persevering.', author: 'Napoleon Bonaparte'},
+    91
+    : 
+    {text: 'Love all, trust a few, do wrong to none.', author: 'William Shakespeare'},
+    92
+    : 
+    {text: 'In order to win, you must expect to win.', author: 'Richard Bach'},
+    93
+    : 
+    {text: 'A goal is a dream with a deadline.', author: 'Napoleon Hill'},
+    94
+    : 
+    {text: 'You can do it if you believe you can!', author: 'Napoleon Hill'},
+    95
+    : 
+    {text: "Set your goals high, and don't stop till you get there.", author: 'Bo Jackson'},
+    96
+    : 
+    {text: 'Every new day is another chance to change your life.', author: null},
+    97
+    : 
+    {text: 'Smile, breathe, and go slowly.', author: 'Thich Nhat Hanh'},
+    98
+    : 
+    {text: 'Nobody will believe in you unless you believe in yourself.', author: 'Liberace'},
+    99
+    : 
+    {text: 'Do more than dream: work.', author: 'William Arthur Ward'},}
+]
+
