1b3e4595f8cc61df0278b2a8a6987998e81f926e
Subhash-K45 <128983855+Subhash-K45@users.noreply.github.com>
2023-03-31 21:29:53 +0530

    second commit

--- Product filtering/index.js
+++ Product filtering/index.js
@@ -1,15 +1,36 @@
-import {data} from './data.js'
-const btn=document.getElementById('btn');
-const author=document.getElementById('Author');
-const Quote=document.getElementById('Quote');
-const Random=(Math.floor(Math.random()*100)).toString()
-Quote.innerText=(data[0][Random].text)
-author.innerText=(data[0][Random].author)
+import {products} from './data.js'
+const input=document.getElementById('input')
+const btn=document.getElementById('btn')
+const container=document.querySelector('.container')
+let arr=[]
+
+for(let i in products){
+    const div=document.createElement('div')
+    const img=document.createElement('img')
+    const name=document.createElement('h3')
+    img.setAttribute('src',products[i].image)
+    name.textContent=products[i].productName
+    name.classList.add("product-name")
+    const price=document.createElement('span')
+    price.textContent=`Price : ${products[i].price}`
+    div.append(img)
+    div.append(name)
+    div.append(price)
+    div.classList.add('product-container')
+    container.append(div)
+}
 btn.addEventListener('click',()=>{
-    const Random=(Math.floor(Math.random()*100)).toString()
-    Quote.innerText=(data[0][Random].text)
-    author.innerText=(data[0][Random].author)
+    const product_name=document.querySelectorAll('.product-name')
+    const div =document.querySelectorAll('.product-container');
+    const input_value=input.value 
+    product_name.forEach((item,i)=>{
+        if(item.textContent.toLowerCase().includes(input_value.toLowerCase())){
+            div[i].classList.remove('hide')
+        }
+        else{
+            div[i].classList.add('hide')
+        }
+    })
+    
 })
-
-
 
