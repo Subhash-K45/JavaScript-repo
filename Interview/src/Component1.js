cf4aae66a7f3cc1204a281460ff951b38485e55c
Subhash-K45 <128983855+Subhash-K45@users.noreply.github.com>
2023-12-03 17:28:02 +0530

    Update Component1.js

--- Interview/src/Component1.js
+++ Interview/src/Component1.js
@@ -1,10 +1,6 @@
 import React, { createContext } from 'react'
 import { useState } from 'react'
 import {Link} from 'react-router-dom'
-import { useEffect } from 'react'
-import { Mycontext } from './App'
-import { useContext } from 'react'
-import { useReducer } from 'react'
 export default function Component1() {
     
     const [user_input,set_user_input] = useState("")
